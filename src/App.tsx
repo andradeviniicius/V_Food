935f9ab96df09afe0b30f92a7cf5bd7870d98624
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-25 17:41:17 -0300

    Implement Router and Sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import MainHeader from "./components/Organisms/MainHeader";
 
 function App() {
   return (
     <div>
+      <MainHeader></MainHeader>
+      <Routes>
+        <Route path="/" element={<Navigate to={'/home'}/>}/>
+      </Routes>
       <h1>Hello world</h1>
     </div>
   );
