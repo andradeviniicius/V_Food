820a4ea7e7615b7b117a6d36c7581133993829b4
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-25 16:36:03 -0300

    Prepare project on typescript

--- src/components/Atoms/comp2/index.tsx
+++ src/components/Atoms/comp2/index.tsx
@@ -1,9 +1,7 @@
-function App() {
+export default function comp2() {
   return (
     <div>
       <h1>Hello world</h1>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Molecules/comp2/index.tsx
@@ -0,0 +1,7 @@
+export default function comp2() {
+  return (
+    <div>
+      <h1>comp2</h1>
+    </div>
+  );
+}
