d12248064aae1a5be50d29cc314dc43515550249
andradeviniicius <viniciusdandrade01@gmail.com>
2022-08-02 17:23:00 -0300

    Create MainFooter for entire application

--- src/components/Organisms/index.ts
+++ src/components/Organisms/index.ts
@@ -1,4 +1,5 @@
 export { default as MainHeader } from "./MainHeader";
+export { default as MainFooter } from "./MainFooter";
 export { default as SearchByCategories } from "./SearchByCategories";
 export { default as FeaturedRecipe } from "./FeaturedRecipe";
 
