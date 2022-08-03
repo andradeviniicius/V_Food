8cb3192675e7db36bf7df69ff2cdbb74466c2bc2
andradeviniicius <viniciusdandrade01@gmail.com>
2022-08-03 17:23:05 -0300

    Install ChakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,18 @@ function App() {
   return (
     <>
       <MainHeader />
-      <main>
-        <Routes>
-          <Route path="/" element={<Navigate to={"/home"} />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/recipes" element={<ErrorMessage />} />
-          <Route path="/blogListPage" element={<ErrorMessage />} />
-          <Route path="/aboutUs" element={<ErrorMessage />} />
-          <Route path="/contact" element={<ErrorMessage />} />
-        </Routes>
-      </main>
+      <ChakraProvider>
+        <main>
+          <Routes>
+            <Route path="/" element={<Navigate to={"/home"} />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/recipes" element={<ErrorMessage />} />
+            <Route path="/blogListPage" element={<ErrorMessage />} />
+            <Route path="/aboutUs" element={<ErrorMessage />} />
+            <Route path="/contact" element={<ErrorMessage />} />
+          </Routes>
+        </main>
+      </ChakraProvider>
       <MainFooter />
     </>
   );
