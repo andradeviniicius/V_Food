55432aabfc46767faf537bd33ef02a9f4c7fdd7c
andradeviniicius <viniciusdandrade01@gmail.com>
2022-08-04 17:41:42 -0300

    Create spacing on the sides for RecipeList

--- src/components/Organisms/RecipeList/index.tsx
+++ src/components/Organisms/RecipeList/index.tsx
@@ -7,26 +7,28 @@ import "./styles.scss";
 const RecipeList = () => {
   return (
     <section className="recipeList">
-      <h1 className="recipeList__title">Simple and Tasty Recipes</h1>
-      <p className="recipeList__subTitle">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
-        magnam!
-      </p>
-      <ChakraProvider>
-        <SimpleGrid placeItems={"center"} columns={[1, 2, 3, 3]} spacing="3%">
-          {database.recipes.slice(0, 9).map((recipe, index) => (
-            <RecipeCard
-              key={recipe.id}
-              image={recipe.image}
-              description={
-                "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem."
-              }
-              categorie={recipe.categorie}
-              prepTime={recipe.prepTime}
-            />
-          ))}
-        </SimpleGrid>
-      </ChakraProvider>
+      <div className="recipeList__content">
+        <h1 className="recipeList__title">Simple and Tasty Recipes</h1>
+        <p className="recipeList__subTitle">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
+          magnam!
+        </p>
+        <ChakraProvider>
+          <SimpleGrid placeItems={"center"} columns={[1, 2, 3, 3]} spacing="3%">
+            {database.recipes.slice(0, 9).map((recipe, index) => (
+              <RecipeCard
+                key={recipe.id}
+                image={recipe.image}
+                description={
+                  "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem."
+                }
+                categorie={recipe.categorie}
+                prepTime={recipe.prepTime}
+              />
+            ))}
+          </SimpleGrid>
+        </ChakraProvider>
+      </div>
     </section>
   );
 };
