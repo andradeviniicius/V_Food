11432ebeac97039d812239f4d08bb0b05f0c06d5
andradeviniicius <viniciusdandrade01@gmail.com>
2022-08-04 12:12:19 -0300

    Tasty Recipes Initial Structure

--- src/components/Organisms/RecipeList/index.tsx
+++ src/components/Organisms/RecipeList/index.tsx
@@ -1,23 +1,32 @@
-import { ChakraProvider, SimpleGrid, Box, Center } from "@chakra-ui/react";
+import { ChakraProvider, SimpleGrid } from "@chakra-ui/react";
 import { RecipeCard } from "components/Atoms";
 import database from "services/dummyDatabase.json";
+
+import "./styles.scss";
 
 const RecipeList = () => {
   return (
     <section className="recipeList">
-      <SimpleGrid placeItems={"center"} columns={[1, 2, 3, 3]} spacing="40px">
-        {database.recipes.map((recipe) => (
-          <RecipeCard
-            key={recipe.id}
-            image={recipe.image}
-            description={
-              "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem."
-            }
-            categorie={recipe.categorie}
-            prepTime={recipe.prepTime}
-          />
-        ))}
-      </SimpleGrid>
+      <h1 className="recipeList__title">Simple and Tasty Recipes</h1>
+      <p className="recipeList__subTitle">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
+        magnam!
+      </p>
+      <ChakraProvider>
+        <SimpleGrid placeItems={"center"} columns={[1, 2, 3, 3]} spacing="3%">
+          {database.recipes.slice(0, 9).map((recipe, index) => (
+            <RecipeCard
+              key={recipe.id}
+              image={recipe.image}
+              description={
+                "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem."
+              }
+              categorie={recipe.categorie}
+              prepTime={recipe.prepTime}
+            />
+          ))}
+        </SimpleGrid>
+      </ChakraProvider>
     </section>
   );
 };
